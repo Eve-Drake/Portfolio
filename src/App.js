c9c230fcca50070cdf4851152089405fa144c4c2
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-08-22 17:37:26 +0100

    first commit portfolio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/Pages/Home';
 import ContactMe from './components/Pages/ContactMe';
 import SmallProjectsPage from './components/Pages/SmallProjectsPage';
