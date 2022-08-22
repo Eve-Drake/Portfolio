889d861d5de6b8c61b9b5fcf6475ea454896669d
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-08-22 18:30:24 +0100

    adding meta viewport

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import BookDetail from './components/Reading-List/BookDetail';
 function App() {
   return (
     <ThemeProvider theme = {theme}>
+
       <Router>
         <div className="App">   
           <BookProvider>
